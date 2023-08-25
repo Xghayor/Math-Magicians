54a0d355e9fe647dcd9c6985b996dae0415dc2e6
Xghayor <cc.baqir@gmail.com>
2023-08-25 01:35:16 +0100

    modified package.json file

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Set the environment variable
-export REACT_APP_NOT_SECRET_CODE=3DPT6zJuhp6ivWqildtvnA==aOy3cSTtq3BUgEhT
-
-# Run the build command
-yarn build
